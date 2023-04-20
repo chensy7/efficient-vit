7d6d08a92366d20529a52238431c9ae942622c0d
Sai Mitheran <49593646+smj007@users.noreply.github.com>
2023-04-19 23:59:57 -0400

    bugfix

--- loader.py
+++ loader.py
@@ -6,6 +6,18 @@ import imageio as m
 from torch.utils import data
 import torchvision.transforms.functional as TF
 from utils import recursive_glob
+from collections import OrderedDict
+
+def convert_state_dict(state_dict):
+    """Converts a state dict saved from a dataParallel module to normal
+    module state_dict inplace
+    :param state_dict is the loaded DataParallel model_state
+    """
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    return new_state_dict
 
 
 class LPCVCLoader(data.Dataset):
